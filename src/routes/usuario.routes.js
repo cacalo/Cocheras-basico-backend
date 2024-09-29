e265063f9e1a75ed2d41f9fbf7466cb5b726acdf
nahuel <nahuel@Xubuntu.myguest.virtualbox.org>
2024-09-29 12:42:25 -0300

    Cambio de authorization para getByUsername

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,7 +5,7 @@ import {methods as authorization} from "./../middlewares/authorization";
 const router=Router();
 
 router.get("/usuarios",authorization.soloAdmin,usuarioController.getAll);
-router.get("/usuarios/:username",authorization.soloAdmin,usuarioController.getByUsername);
+router.get("/usuarios/:username",authorization.soloPrivate,usuarioController.getByUsername);
 // router.get("/usuarios/:id",authorization.soloAdmin,usuarioController.getById);
 router.put("/usuarios/:username",authorization.soloAdmin,usuarioController.update);
 router.delete("/usuarios/:username",authorization.soloAdmin,usuarioController.softDelete);
